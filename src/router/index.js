c43ac8be20b1aae54ed573ae3f0fb5ffc33ce1f5
KevinTortoledoRodriguez <60956746+KevinTortoledoRodriguez@users.noreply.github.com>
2021-07-30 03:23:12 -0600

    Avances incompletos

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Agregar from '../views/Agregar'
+import Detalle from '../views/Detalle'
+import Editar from '../views/Editar'
+import Inicio from '../views/Inicio'
+import Visualizar from '../views/Visualizar'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Inicio',
+    component: Inicio
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/visualizar',
+    name: 'Visualizar',
+    component: Visualizar
+  },
+  {
+    path: '/detalle',
+    name: 'Detalle',
+    component: Detalle
+  },
+  {
+    path: '/editar',
+    name: 'Editar',
+    component: Editar
+  },
+  {
+    path: '/agregar',
+    name: 'Agregar',
+    component: Agregar
   }
 ]
 
